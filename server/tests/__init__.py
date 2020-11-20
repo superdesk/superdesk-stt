a79a12d05efdc54bbab34d25ba4e0e81d2aed2d6
olegpshenichniy <olegpshenichniy@gmail.com>
2020-11-20 16:39:05 +0100

    Do not cut off html link from body html during ingest. Replace pre with p.

---
+++ server/tests/__init__.py
@@ -0,0 +1,27 @@
+import os
+
+from lxml import etree
+
+from superdesk.tests import TestCase as CoreTestCase
+from stt.parser import STTParser
+
+
+class TestCase(CoreTestCase):
+
+    fixture = None
+
+    def setUp(self):
+        dirname = os.path.dirname(os.path.realpath(__file__))
+        fixture = os.path.join(dirname, 'fixtures', self.fixture)
+        provider = {'name': 'Test'}
+        with self.ctx:
+            with open(fixture, 'rb') as f:
+                parser = STTParser()
+                self.xml_root = etree.parse(f).getroot()
+                self.item = parser.parse(self.xml_root, provider)[0]
+
+    def setUpForChildren(self):
+        super().setUpForChildren()
+        # stt related configs
+        self.app.config['HTML_TAGS_WHITELIST'] = ('h1', 'h2', 'h3', 'h4', 'h6', 'blockquote', 'figure', 'ul', 'ol',
+                                                  'li', 'div', 'p', 'em', 'strong', 'i', 'b', 'a', 'pre')
