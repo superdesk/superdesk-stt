551c3ae53985f4bbdc68a148896f72edb2f40344
Mark Pittaway <mark.pittaway@gmail.com>
2022-05-24 15:42:43 +1000

    fix parser

--- server/stt/stt_events_ml.py
+++ server/stt/stt_events_ml.py
@@ -1,3 +1,5 @@
+from xml.etree.ElementTree import Element
+
 from superdesk.utc import local_to_utc
 from superdesk.io.registry import register_feed_parser
 from planning.feed_parsers.events_ml import EventsMLParser
@@ -6,14 +8,21 @@ TIMEZONE = "Europe/Helsinki"
 
 
 class STTEventsMLParser(EventsMLParser):
-    NAME = 'stteventsml'
-    label = 'STT Events ML'
+    NAME = "stteventsml"
+    label = "STT Events ML"
 
     SUBJ_QCODE_PREFIXES = {
         "stt-subj": None,
         "sttdepartment": "sttdepartment",
         "sttsubj": "sttsubj",
     }
+
+    def parse(self, tree: Element, provider=None):
+        items = super(STTEventsMLParser, self).parse(tree, provider)
+        for item in items:
+            self.set_extra_fields(item, tree)
+
+        return items
 
     def datetime(self, value):
         """When there is no timezone info, assume it's Helsinki timezone."""
@@ -25,15 +34,24 @@ class STTEventsMLParser(EventsMLParser):
     def set_extra_fields(self, item, xml):
         """Adds extra fields"""
 
+        concept = xml.find(self.qname("concept"))
+
+        # Add ``sttevents``, if one found
+        try:
+            values = concept.find(self.qname("conceptId")).get("qcode", "").split(":")
+            if values and values[0] == "sttevents":
+                item.setdefault("extra", {})["stt_events"] = values[1]
+        except AttributeError:
+            pass
+
         # Add ``stt-topics``, if one found
         try:
-            for subject in xml.find(self.qname("contentMeta")).findall(self.qname("subject")):
-                values = subject.get('qcode', '').split(':')
-                if values:
-                    if values[0] == 'stt-topics':
-                        item.setdefault('extra', {})['stt_topics'] = values[1]
+            for subject in concept.find(self.qname("eventDetails")).findall(self.qname("subject")):
+                values = subject.get("qcode", "").split(":")
+                if values and values[0] == "stt-topics":
+                    item.setdefault("extra", {})["stt_topics"] = values[1]
         except AttributeError:
             pass
 
 
-register_feed_parser(STTEventsMLParser.NAME, STTEventsMLParser)
+register_feed_parser(STTEventsMLParser.NAME, STTEventsMLParser())
