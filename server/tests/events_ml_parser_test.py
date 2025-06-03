c43df70908919f4e885e18c9a8266139b4f1a321
Brian Mwangi <mwangikabiru21@gmail.com>
2025-06-03 11:18:12 +0300

    Fixed test

--- server/tests/events_ml_parser_test.py
+++ server/tests/events_ml_parser_test.py
@@ -50,7 +50,7 @@ class STTEventsMLParserTest(TestCase):
         self.assertEqual(location["address"]["extra"]["sttcountry"], "1")
         self.assertEqual(location["address"]["extra"]["iso3166"], "iso3166-1a2:FI")
         self.assertEqual(location["address"]["line"][0], "Eteläinen Rautatiekatu 4")
-        self.assertEqual(location["details"], ["Knock 3 times"])
+        self.assertEqual(location["details"], "Knock 3 times")
 
 
 class STTEventsMLParserEventTypeCVTest(TestCase):
