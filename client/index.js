ccbe857e7edf49379100b1b248aeffd2400cdf83
Konstantin Markov <maga.ot.oz@protonmail.com>
2025-08-29 14:11:09 +0300

    Remove custom config

--- client/index.js
+++ client/index.js
@@ -7,10 +7,6 @@ setTimeout(() => {
   startApp(
     [
       {
-        id: 'custom-config-extension',
-        load: () => import('./stt/custom-config'),
-      },
-      {
         id: 'planning-extension',
         load: () => import('superdesk-planning/client/planning-extension'),
         configuration: {
