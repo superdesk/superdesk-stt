05feb16fb61434396026f5678d65d7bc8d44059c
Brian Mwangi <mwangikabiru21@gmail.com>
2025-09-10 11:26:37 +0300

    Added function to match pirority qcodes with stturgency value names in output

--- server/stt/stt_newsroom_ninjs_formatter.py
+++ server/stt/stt_newsroom_ninjs_formatter.py
@@ -1,5 +1,6 @@
 import logging
 
+from superdesk import get_resource_service
 from superdesk.publish.formatters import NewsroomNinjsFormatter
 
 logger = logging.getLogger(__name__)
@@ -13,6 +14,43 @@ class STTNewsroomNinjsFormatter(NewsroomNinjsFormatter):
         self.can_preview = False
         self.can_export = False
         self.internal_renditions = ["original", "viewImage", "baseImage"]
+
+    def update_ninjs_stt_urgency(self, ninjs):
+        priority = ninjs.get("priority", 0)
+
+        if priority == 1:
+            qcode = "stturgency-1"
+        elif priority == 2 or priority == 3:
+            # Special case where qcode 2 and 3 mapped to value of qcode 2 to NH
+            qcode = "stturgency-2"
+        elif priority == 4:
+            qcode = "stturgency-4"
+        else:
+            qcode = "stturgency-5"
+
+        items = get_resource_service("vocabularies").get_items("stturgency")
+
+        stturgency_item = None
+        for item in items:
+            if item.get("qcode") == qcode:
+                stturgency_item = item
+                break
+
+        if stturgency_item:
+            if "subject" not in ninjs:
+                ninjs["subject"] = []
+
+            ninjs["subject"] = [
+                subj for subj in ninjs["subject"] if subj.get("scheme") != "stturgency"
+            ]
+
+            ninjs["subject"].append(
+                {
+                    "name": stturgency_item["name"],
+                    "qcode": qcode,
+                    "scheme": "stturgency",
+                }
+            )
 
     def update_ninjs_stt_sources(self, ninjs):
         try:
@@ -41,5 +79,6 @@ class STTNewsroomNinjsFormatter(NewsroomNinjsFormatter):
     async def _transform_to_ninjs(self, article, subscriber, recursive=True):
         ninjs = await super()._transform_to_ninjs(article, subscriber, recursive)
         self.update_ninjs_stt_sources(ninjs)
+        self.update_ninjs_stt_urgency(ninjs)
 
         return ninjs
