eb1c3cd6338e49555fdca943f840b4288ad30e81
Brian Mwangi <mwangikabiru21@gmail.com>
2025-10-28 15:59:23 +0300

    Added tests and new fixture file for STT JSON Planning

--- server/tests/stt_json_planning_formatter_test.py
+++ server/tests/stt_json_planning_formatter_test.py
@@ -1,3 +1,5 @@
+import json
+import os
 from tests import TestCase
 from superdesk import get_resource_service
 from stt.stt_json_planning_formatter import STTJsonPlanningFormatter
@@ -6,6 +8,12 @@ from stt.stt_json_planning_formatter import STTJsonPlanningFormatter
 class STTJsonPlanningFormatterTest(TestCase):
     add_stt_cvs = True
     parse_source = False
+    fixture = "json/stt_json_planning_item.json"
+
+    def get_sample_item(self):
+        fixture_path = os.path.join(os.path.dirname(__file__), "fixtures", self.fixture)
+        with open(fixture_path, "r") as f:
+            return json.load(f)
 
     async def test_update_stt_urgency_mappings(self):
         async with self.app.app_context():
@@ -28,3 +36,82 @@ class STTJsonPlanningFormatterTest(TestCase):
                 self.assertEqual(subj["qcode"], expected_qcode)
                 self.assertEqual(subj["scheme"], "stturgency")
                 self.assertEqual(subj["name"], name_map[expected_qcode])
+
+    async def test_exclude_internal_planning_fields(self):
+        async with self.app.app_context():
+            formatter = STTJsonPlanningFormatter()
+            item = self.get_sample_item()
+
+            self.assertIn("internal_note", item)
+            self.assertTrue(
+                any(
+                    subj.get("scheme") == "sttcheckedby"
+                    for subj in item.get("subject", [])
+                )
+            )
+
+            formatter.exclude_internal_planning_fields(item)
+
+            self.assertNotIn("internal_note", item)
+            self.assertFalse(
+                any(
+                    subj.get("scheme") == "sttcheckedby"
+                    for subj in item.get("subject", [])
+                )
+            )
+            self.assertTrue(
+                any(
+                    subj.get("scheme") == "stturgency"
+                    for subj in item.get("subject", [])
+                )
+            )
+
+    async def test_exclude_internal_coverage_fields(self):
+        async with self.app.app_context():
+            formatter = STTJsonPlanningFormatter()
+            item = self.get_sample_item()
+
+            coverage = item["coverages"][0]
+            planning = coverage["planning"]
+            self.assertIn("headline", planning)
+            self.assertIn("location", planning)
+            self.assertIn("internal_note", planning)
+            self.assertIn("scheduled", planning)
+            fields = planning["fields"]
+            self.assertTrue(
+                any(field["field"] == "sttpicturewhatabout" for field in fields)
+            )
+            self.assertTrue(
+                any(field["field"] == "sttregistrationinfo" for field in fields)
+            )
+
+            formatter.exclude_internal_coverage_fields(item)
+
+            self.assertNotIn("headline", planning)
+            self.assertNotIn("location", planning)
+            self.assertNotIn("internal_note", planning)
+            self.assertIn("scheduled", planning)
+            fields_after = planning["fields"]
+            self.assertFalse(
+                any(field["field"] == "sttpicturewhatabout" for field in fields_after)
+            )
+            self.assertFalse(
+                any(field["field"] == "sttregistrationinfo" for field in fields_after)
+            )
+
+    async def test_map_agendas(self):
+        async with self.app.app_context():
+            formatter = STTJsonPlanningFormatter()
+            item = self.get_sample_item()
+
+            self.assertEqual(item["agendas"], [])
+
+            formatter.map_agenda_output(item)
+            expected_agendas = [
+                {
+                    "_id": "Julkishallinnon tiedotepalvelu",
+                    "name": "Julkishallinnon tiedotepalvelu",
+                },
+                {"_id": "Muut kuin urheilu", "name": "Muut kuin urheilu"},
+            ]
+            self.assertEqual(item["agendas"], expected_agendas)
