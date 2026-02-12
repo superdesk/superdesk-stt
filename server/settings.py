1d59a863fbb1df9810e587b101213a982af14e73
Konstantin Markov <maga.ot.oz@protonmail.com>
2026-02-12 14:49:20 +0200

    Allow embargoed items update (#701)

--- server/settings.py
+++ server/settings.py
@@ -386,3 +386,5 @@ if SAML_PATH and os.path.exists(SAML_PATH) and not strtobool(env("SUPERDESK_AUTH
 
 # Exclude Desks in Planning json formatter
 PLANNING_JSON_EXCLUDE_ASSIGNEE_FIELDS = ["desk"]
+
+ALLOW_UPDATING_EMBARGOED_ITEMS = True
