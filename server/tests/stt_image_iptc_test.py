a9cb93b5dbf6ab735fd6e4c84a37a88637a49576
msilven <mikko.silvennoinen@stt-lehtikuva.fi>
2026-03-24 07:57:51 +0000

    Add unit tests to fix/stt-image-parser

--- server/tests/stt_image_iptc_test.py
+++ server/tests/stt_image_iptc_test.py
@@ -7,6 +7,7 @@
 # For the full copyright and license information, please see the
 # AUTHORS and LICENSE files distributed with this source code, or
 # at https://www.sourcefabric.org/superdesk/license
+#
 
 import unittest
 from datetime import datetime
